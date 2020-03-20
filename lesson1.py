5f754385df0a3dfbc2effdd8ec44890ea73bb1bb
Ricky Martinez <martinez.ricky95@gmail.com>
2020-03-20 15:06:52 -0400

    Lesson 1 on learning about lists and conditionals

---
+++ lesson1.py
@@ -0,0 +1,31 @@
+#/usr/local/bin/python3
+
+#NOTE: DATASTRUCTURES
+
+#NOTE: INITIALIZE innteer var
+I = 10
+
+#NOTE: INITIALIZE empty list
+J = []
+
+#NOTE: INITIALIZE populated list
+k = [1, 2, 3, 4, 5, 'ricky']
+
+a = True
+if a == True:
+    print("Hi")
+    print("Hi")
+    print("Hi")
+    print("Hi")
+    print("Hi")
+    print("Hi")
+    print("Hi")
+    print("Hi")
+    print("Hi")
+
+    a = False
+
+if a == True: 
+    print("A still true")
+
+print("Bye")
